c53908fc0515b4957f5fbf0b8f974cf116853bae
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-05-29 22:44:06 -0300

    Diseño de pantalla de Héroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap, tap } from 'rxjs';
 import { HeroesService } from '../../services/heroes.service';
 import { Heroe } from '../../interfaces/heroes.interface';
@@ -7,18 +7,30 @@ import { Heroe } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [],
+  styles: [
+    `
+      img {
+        width: 100%;
+        border-radius: 10px;
+      }
+    `,
+  ],
 })
 export class HeroeComponent implements OnInit {
   heroe!: Heroe;
   constructor(
     private _activatedRoute: ActivatedRoute,
-    private _heroesService: HeroesService
+    private _heroesService: HeroesService,
+    private _route: Router
   ) {}
 
   ngOnInit(): void {
     this._activatedRoute.params
       .pipe(switchMap(({ id }) => this._heroesService.getHeroePorId(id)))
       .subscribe((heroe: Heroe) => (this.heroe = heroe));
+  }
+
+  regresar() {
+    this._route.navigate(['/heroes/listado']);
   }
 }
--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -2,12 +2,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
 })
 export class ImagenPipe implements PipeTransform {
-
   transform(value: Heroe): string {
-    return `assets/heroes/${value.id}.jpg`
+    return `assets/heroes/${value.id}.jpg`;
   }
-
 }
